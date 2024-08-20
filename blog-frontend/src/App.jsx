3106031aac422557f124b512f89d9593a47e06ea
ptwelephnt <anthony.leggett.al@gmail.com>
2024-08-20 16:06:57 -0700

    Created vite project

---
+++ blog-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import BlogList from './components/blog-list/blog-list.component'
+
+function App() {
+
+  return (
+    <>
+      <BlogList />
+    </>
+  )
+}
+
+export default App
---
+++ blog-frontend/src/components/blog-list/blog-list.component.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import './blog-list.styles.scss';
+
+const BlogList = () => {
+    
+    return (
+        <div>
+            <h1>Hi</h1>
+        </div>
+    )
+}
+
+export default BlogList;
